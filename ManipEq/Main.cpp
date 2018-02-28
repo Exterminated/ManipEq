5dd423a7b841aec718e82aca88acc50191a37bc8
Девяткин Федор <devjatkin.fjodr@gmail.com>
2018-02-28 11:38:43 +0300

    Прикручена минимизация

--- ManipEq/Main.cpp
+++ ManipEq/Main.cpp
@@ -1,37 +1,63 @@
 #define _USE_MATH_DEFINES
+
 #include "cmath"
 #include <conio.h>
 #include <iostream>
+#include "alglib\optimization.h"
 
 using namespace std;
+using namespace alglib;
+
+double xb, y_0, za, xc, yb, tc, zd, O1A, OA1, l1, l2, l3, OO1;
+double  fi_angle, l4, A, gamma1, gamma0, gamma, xm0, ym0, zm0, xe0, ye0, ze0, psi1, psi2, alpha;
+double xmk, ymk, zmk, xek, yek, zek;
+int alpha_0, alpha_23, a, alpha_13, alpha_33, b;
+double OK, OA, OB, DK, Imin;
+
+void  function1_fvec(const real_1d_array &x, real_1d_array &fi, void *ptr)
+{	
+	// this callback calculates
+	
+	fi[0] = pow((sqrt((xmk*xmk + (ymk + OA1*sin(fi_angle))*(ymk + OA1*sin(fi_angle)) + ((zmk - OA1*cos(fi_angle))*(zmk - OA1*cos(fi_angle))))) - l1), 2.0);
+	fi[1] = pow((sqrt((OK - OA1*sin(fi_angle))*(OK - OA1*sin(fi_angle)) + (OA1*cos(fi_angle) - DK)*(OA1*cos(fi_angle) - DK)) - l4), 2.0);
+
+	//fi[0] = 10 * pow(x[0] + 3, 2);
+	//fi[1] = pow(x[1] - 3, 2);
+	
+}
 
 void main() {
-	double xb, y0, za, xc, yb, tc, zd, O1A, OA1, l1, l2, l3, OO1;
-	double  fi, l4, A, gamma1, gamma0, gamma, xm0, ym0, zm0, xe0, ye0, ze0, psi1, psi2, alpha;
-	double xmk, ymk, zmk, xek, yek, zek;
-	int alpha_0, alpha_23, a, alpha_13, alpha_33, b;
+	real_1d_array x = "[0,0]";
+	//double epsg = 0.0000000001;
+	double epsg = 0.002;
+	double epsf = 0;
+	double epsx = 0;
+	ae_int_t maxits = 0;
+	minlmstate state;
+	minlmreport rep;
+
 	//Исходные конструктивные данные
 	xb = 355.0;
-	y0 = 755.0;
+	y_0 = 755.0;
 	za = 750.0;
 	xc = -355.0;
-	yb = y0;
+	yb = y_0;
 	zd = -40.0;
 	O1A = za;
 	OA1 = 750.0;
 	l1 = 1400.0;
 	l2 = 1500.0;
 	l3 = 1352.0;
-	fi = 0.323;
+	fi_angle = 0.323;
 	OO1 = zd;
 
 	//Начальные координаты точки М и длина 4 звена
-	l4 = sqrt((za*sin(M_PI_2 - fi) - OO1)*(za*sin(M_PI_2 - fi) - OO1) + (yb - za*cos(M_PI_2 - fi))*(yb - za*cos(M_PI_2 - fi)));
+	l4 = sqrt((za*sin(M_PI_2 - fi_angle) - OO1)*(za*sin(M_PI_2 - fi_angle) - OO1) + (yb - za*cos(M_PI_2 - fi_angle))*(yb - za*cos(M_PI_2 - fi_angle)));
 	A = 0.5*l2*l2 + 0.5*l3*l3 - l1*l1;
 	xm0 = (l3*l3 - l2*l2) / (4 * xb);	
-	ym0 = sqrt((l1*l1) - (((l3*l3 - l2*l2)*(l3*l3 - l2*l2)) / (16 * xb * xb)) - (((A - xb*xb - za*za)*(A - xb*xb - za*za)) / (4 * za*za)))*cos(fi) - ((A - xb*xb + za*za) / (2 * za))*sin(fi);
-	zm0 = sqrt((l1*l1) - (((l3*l3 - l2*l2)*(l3*l3 - l2*l2)) / (16 * xb * xb)) - (((A - xb*xb - za*za)*(A - xb*xb - za*za)) / (4 * za*za)))*sin(fi) + ((A - xb*xb + za*za) / (2 * za))*cos(fi);
-	gamma0 = atan((-1 * xm0) / (ym0 + OA1*sin(fi)));
+	ym0 = sqrt((l1*l1) - (((l3*l3 - l2*l2)*(l3*l3 - l2*l2)) / (16 * xb * xb)) - (((A - xb*xb - za*za)*(A - xb*xb - za*za)) / (4 * za*za)))*cos(fi_angle) - ((A - xb*xb + za*za) / (2 * za))*sin(fi_angle);
+	zm0 = sqrt((l1*l1) - (((l3*l3 - l2*l2)*(l3*l3 - l2*l2)) / (16 * xb * xb)) - (((A - xb*xb - za*za)*(A - xb*xb - za*za)) / (4 * za*za)))*sin(fi_angle) + ((A - xb*xb + za*za) / (2 * za))*cos(fi_angle);
+	gamma0 = atan((-1 * xm0) / (ym0 + OA1*sin(fi_angle)));
 	cout << "l4: " << l4 << endl;
 	cout << "A: " << A << endl;
 	cout << "xm0: " << xm0 << endl;
@@ -80,16 +106,22 @@ void main() {
 	cout << "zmk: " << zmk << endl;
 	
 	//Оптимизация угла фи
-	double OK, OA, OB, DK, Imin;
-	OK = y0;
+	
+	OK = y_0;
 	OA = za;
 	OB = xb;
 	DK = zd;
 	OA1 = za;
-	
-	Imin = pow((sqrt((xmk*xmk + (ymk + OA1*sin(fi))*(ymk + OA1*sin(fi)) + ((zmk - OA1*cos(fi))*(zmk - OA1*cos(fi))))) - l1), 2.0) + pow((sqrt((OK - OA1*sin(fi))*(OK - OA1*sin(fi)) + (OA1*cos(fi) - DK)*(OA1*cos(fi) - DK)) - l4), 2.0);
 
-	cout << "Imin(fi): " << Imin << endl;
+	minlmcreatev(2, x, 0.002, state);
+	minlmsetcond(state, epsg, epsf, epsx, maxits);
+	minlmoptimize(state, function1_fvec);
+	minlmresults(state, x, rep);
+	
+	//Imin = pow((sqrt((xmk*xmk + (ymk + OA1*sin(fi))*(ymk + OA1*sin(fi)) + ((zmk - OA1*cos(fi))*(zmk - OA1*cos(fi))))) - l1), 2.0) + pow((sqrt((OK - OA1*sin(fi))*(OK - OA1*sin(fi)) + (OA1*cos(fi) - DK)*(OA1*cos(fi) - DK)) - l4), 2.0);
+
+	cout << int(rep.terminationtype) << endl;
+	cout << x.tostring(2).c_str() << endl;
 
 	_getch();
 }
